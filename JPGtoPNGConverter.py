c41bef82b0ef1e50b455a8114b04418de1f1b9ec
Tsuyoshi Koyama <tsuyoshikoyama2525@gmail.com>
2022-07-21 16:06:56 -0400

    Implemented jpg converter using pillow

--- JPGtoPNGConverter.py
+++ JPGtoPNGConverter.py
@@ -1,1 +1,19 @@
+import sys
+import os
+from PIL import Image
 
+# Example to run: # python3 JOGtoPNGconverter.py folder newFolder
+
+# get first and second argument
+from_folder = sys.argv[1]
+to_folder = sys.argv[2]
+
+# check if newFolder exists, if not create it
+if not os.path.isdir(to_folder):
+    os.mkdir(to_folder)
+
+# converts each image to png and saves to new folder
+for filename in os.listdir(from_folder):
+    image = Image.open(f"{from_folder}/{filename}")
+    name = filename.split(".")[0]
+    image.save(f"{to_folder}/{name}.png", "png")
